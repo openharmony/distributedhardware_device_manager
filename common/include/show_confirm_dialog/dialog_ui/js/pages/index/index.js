2ebed52bb65a2fa1ed4267017a26e641e12207a0
wuqi0105 <wuqi85@huawei.com>
2022-03-24 15:44:20 +0800

    Timer 异常
    Signed-off-by: wuqi0105 <wuqi85@huawei.com>

--- common/include/show_confirm_dialog/dialog_ui/js/pages/index/index.js
+++ common/include/show_confirm_dialog/dialog_ui/js/pages/index/index.js
@@ -25,12 +25,6 @@ var EVENT_INIT_CODE = "2";
 export default {
     data: {
         seconds:60,
-        EVENT_CONFIRM:"EVENT_CONFIRM",
-        EVENT_CANCEL:"EVENT_CANCEL",
-        EVENT_INIT:"EVENT_INIT",
-        EVENT_CONFIRM_CODE:"0",
-        EVENT_CANCEL_CODE:"1",
-        EVENT_INIT_CODE:"2",
     },
     onInit() {
         console.info('getParams: ' + router.getParams());
@@ -38,10 +32,15 @@ export default {
     },
     onShow() {
         timel = setInterval(this.run,1000)
-        clearInterval(timel);
-        timel = null;
-        console.info('click cancel');
-        callNativeHandler(EVENT_CANCEL, EVENT_CANCEL_CODE);
+    },
+    run() {
+        this.seconds--;
+        if (this.seconds == 0) {
+            clearInterval(timel);
+            timel = null;
+            console.info('click cancel');
+            callNativeHandler(EVENT_CANCEL, EVENT_CANCEL_CODE);
+        }
     },
     onConfirm() {
         console.info('click confirm');
