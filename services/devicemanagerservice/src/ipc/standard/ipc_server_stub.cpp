f0e92fd1707f60f825f7e37629f03f3aa7880883
wangyb0625 <wangyibo38@huawei.com>
2022-05-24 16:54:19 +0800

    codecheck
    Signed-off-by: wangyb0625 <wangyibo38@huawei.com>

--- services/devicemanagerservice/src/ipc/standard/ipc_server_stub.cpp
+++ services/devicemanagerservice/src/ipc/standard/ipc_server_stub.cpp
@@ -84,7 +84,7 @@ int32_t IpcServerStub::OnRemoteRequest(uint32_t code, MessageParcel &data, Messa
     auto remoteDescriptor = data.ReadInterfaceToken();
     if (GetDescriptor() != remoteDescriptor) {
         LOGI("ReadInterfaceToken fail!");
-        return ERR_INVALID_STATE;
+        return ERR_DM_IPC_READ_TOKEN_FAILED;
     }
 
     int32_t ret = IpcCmdRegister::GetInstance().OnIpcCmd((int32_t)code, data, reply);
