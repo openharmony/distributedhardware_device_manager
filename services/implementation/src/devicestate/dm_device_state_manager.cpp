873cf5a094137926325098a4319d20375d3d0778
summer8999 <xionghuan7@huawei.com>
2022-09-12 10:09:35 +0800

    adapt db ondeviceready
    Signed-off-by: summer8999 <xionghuan7@huawei.com>

--- services/implementation/src/devicestate/dm_device_state_manager.cpp
+++ services/implementation/src/devicestate/dm_device_state_manager.cpp
@@ -76,6 +76,7 @@ void DmDeviceStateManager::DeleteOfflineDeviceInfo(const std::string &pkgName, c
     for (auto iter: remoteDeviceInfos_) {
         if (iter.second.deviceId == info.deviceId) {
             remoteDeviceInfos_.erase(iter.first);
+            LOGI("DeleteOfflineDeviceInfo compelete, deviceId = %s", GetAnonyString(std::string(info.deviceId)).c_str());
             break;
         }
     }
