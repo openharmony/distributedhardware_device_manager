91322f449a7ff823e9d48c262601604d12f887ee
summer8999 <xionghuan7@huawei.com>
2022-09-19 20:34:56 +0800

    safty check
    Signed-off-by: summer8999 <xionghuan7@huawei.com>

--- services/implementation/include/discovery/dm_discovery_filter.h
+++ services/implementation/include/discovery/dm_discovery_filter.h
@@ -31,7 +31,7 @@ struct DmDeviceFilters {
 struct DmDeviceFilterOption {
     std::string filterOp_;
     std::vector<DmDeviceFilters> filters_;
-    int32_t ParseFilterJson(const std::string &str, nlohmann::json &jsonObject);
+    int32_t ParseFilterJson(const std::string &str);
     int32_t TransformToFilter(const std::string &filterOptions);
 };
 
