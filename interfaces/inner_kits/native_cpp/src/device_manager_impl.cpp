6eb54faf8f5e7f89be35cea1ea7f4b4d2365f98b
summer8999 <xionghuan7@huawei.com>
2022-09-21 14:55:10 +0800

    modify OnRemoteDied
    Signed-off-by: summer8999 <xionghuan7@huawei.com>

--- interfaces/inner_kits/native_cpp/src/device_manager_impl.cpp
+++ interfaces/inner_kits/native_cpp/src/device_manager_impl.cpp
@@ -766,7 +766,7 @@ int32_t DeviceManagerImpl::NotifyEvent(const std::string &pkgName, const int32_t
 int32_t DeviceManagerImpl::HandleDmServiceDied(const wptr<IRemoteObject> &remote)
 {
     LOGI("DeviceManager::HandleDmServiceDied begin");
-    int32_t ret = IpcClientProxy_->OnDmServiceDied(remote);
+    int32_t ret = ipcClientProxy_->OnDmServiceDied(remote);
     if (ret != DM_OK) {
         LOGE("DeviceManager::HandleDmServiceDied failed, ret: %d", ret);
         return ERR_DM_FAILED;
