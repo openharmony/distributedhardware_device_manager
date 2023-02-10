4d00047ec75aa7cee1491d560ef2f827be5d8c7d
@shi-xiaoxiao-iris <shixiaoxiao2@huawei.com>
2023-02-10 09:43:35 +0800

    GetTrustedDeviceList networkid上报
    Signed-off-by: @shi-xiaoxiao-iris <shixiaoxiao2@huawei.com>

--- interfaces/kits/js/src/native_devicemanager_js.cpp
+++ interfaces/kits/js/src/native_devicemanager_js.cpp
@@ -754,10 +754,10 @@ void DeviceManagerNapi::DeviceInfoToJsArray(const napi_env &env, const std::vect
     napi_create_object(env, &result);
 
     SetValueUtf8String(env, "deviceId", vecDevInfo[idx].deviceId, result);
+    SetValueUtf8String(env, "networkId", vecDevInfo[idx].networkId, result);
     SetValueUtf8String(env, "deviceName", vecDevInfo[idx].deviceName, result);
     SetValueInt32(env, "deviceType", (int)vecDevInfo[idx].deviceTypeId, result);
     SetValueInt32(env, "authForm", (int)vecDevInfo[idx].authForm, result);
-    SetValueUtf8String(env, "networkId", vecDevInfo[idx].networkId, result);
 
     napi_status status = napi_set_element(env, arrayResult, idx, result);
     if (status != napi_ok) {
