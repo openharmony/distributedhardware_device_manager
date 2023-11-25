80c8c1ec78f896005ad929a54f034aa4c147ad6c
wangxuanxuan <wangxuanxuan9@huawei.com>
2023-11-25 10:00:31 +0800

    modify on discovery failed lock
    Signed-off-by: wangxuanxuan <wangxuanxuan9@huawei.com>

--- services/implementation/src/dependency/softbus/softbus_connector.cpp
+++ services/implementation/src/dependency/softbus/softbus_connector.cpp
@@ -546,7 +546,7 @@ void SoftbusConnector::OnSoftbusDiscoveryResult(int subscribeId, RefreshResult r
     } else {
         LOGE("[SOFTBUS]fail to discovery device with subscribeId: %d, result: %d.", subscribeId, result);
         for (auto iter = discoveryCallbackMap_.begin(); iter != discoveryCallbackMap_.end();) {
-            iter.second->OnDiscoveryFailed(iter.first, originId, result);
+            iter->second->OnDiscoveryFailed(iter->first, originId, result);
             iter = discoveryCallbackMap_.erase(iter);
         }
     }
