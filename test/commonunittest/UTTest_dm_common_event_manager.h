f9c828e853bf7122de702a7b70c8a7da0867f86f
qinlong <qinlong18@huawei.com>
2024-04-15 02:57:05 +0000

    update test/commonunittest/UTTest_dm_common_event_manager.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/commonunittest/UTTest_dm_common_event_manager.h
+++ test/commonunittest/UTTest_dm_common_event_manager.h
@@ -28,8 +28,8 @@ class DmCommonEventManagerTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 };
 } // namespace DistributedHardware
 } // namespace OHOS
