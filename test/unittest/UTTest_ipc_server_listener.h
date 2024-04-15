1aa17c0be5bad311b84fe40b281204549f1cc037
qinlong <qinlong18@huawei.com>
2024-04-15 03:08:17 +0000

    update test/unittest/UTTest_ipc_server_listener.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/unittest/UTTest_ipc_server_listener.h
+++ test/unittest/UTTest_ipc_server_listener.h
@@ -28,8 +28,8 @@ class IpcServerListenerTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 };
 } // namespace DistributedHardware
 } // namespace OHOS
