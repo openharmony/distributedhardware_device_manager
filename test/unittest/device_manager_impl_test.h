c5eedc8bbb6739e541d07e63dfdd29635f220753
qinlong <qinlong18@huawei.com>
2024-04-15 03:09:50 +0000

    update test/unittest/device_manager_impl_test.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/unittest/device_manager_impl_test.h
+++ test/unittest/device_manager_impl_test.h
@@ -30,8 +30,8 @@ class DeviceManagerImplTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 
 private:
     std::shared_ptr<DiscoveryCallback> test_callback_ = nullptr;
