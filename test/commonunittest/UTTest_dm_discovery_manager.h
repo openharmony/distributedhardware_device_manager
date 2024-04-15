be0860925d1a164f669beceb8a9d89388c13bd87
qinlong <qinlong18@huawei.com>
2024-04-15 02:57:51 +0000

    update test/commonunittest/UTTest_dm_discovery_manager.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/commonunittest/UTTest_dm_discovery_manager.h
+++ test/commonunittest/UTTest_dm_discovery_manager.h
@@ -33,8 +33,8 @@ class DmDiscoveryManagerTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 };
 } // namespace DistributedHardware
 } // namespace OHOS
