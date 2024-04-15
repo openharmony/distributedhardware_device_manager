94678e3110c2a3ed7eac2febe7e9c77441803a8e
qinlong <qinlong18@huawei.com>
2024-04-15 03:07:05 +0000

    update test/unittest/UTTest_ipc_cmd_register.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/unittest/UTTest_ipc_cmd_register.h
+++ test/unittest/UTTest_ipc_cmd_register.h
@@ -37,8 +37,8 @@ class IpcCmdRegisterTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 };
 } // namespace DistributedHardware
 } // namespace OHOS
