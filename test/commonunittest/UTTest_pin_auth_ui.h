23c0b4e15cce988ab4d48955e33035da01a15c12
qinlong <qinlong18@huawei.com>
2024-04-15 03:00:21 +0000

    update test/commonunittest/UTTest_pin_auth_ui.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/commonunittest/UTTest_pin_auth_ui.h
+++ test/commonunittest/UTTest_pin_auth_ui.h
@@ -26,8 +26,8 @@ class PinAuthUiTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 };
 } // namespace DistributedHardware
 } // namespace OHOS
