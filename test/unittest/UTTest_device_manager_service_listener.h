ba1597753b69d1372f2c748afb1ccdb617334175
qinlong <qinlong18@huawei.com>
2024-04-15 03:03:53 +0000

    update test/unittest/UTTest_device_manager_service_listener.h.
    Signed-off-by: qinlong <qinlong18@huawei.com>

--- test/unittest/UTTest_device_manager_service_listener.h
+++ test/unittest/UTTest_device_manager_service_listener.h
@@ -29,8 +29,8 @@ class DeviceManagerServiceListenerTest : public testing::Test {
 public:
     static void SetUpTestCase();
     static void TearDownTestCase();
-    void SetUp() override;
-    void TearDown() override;
+    void SetUp();
+    void TearDown();
 };
 } // namespace DistributedHardware
 } // namespace OHOS
