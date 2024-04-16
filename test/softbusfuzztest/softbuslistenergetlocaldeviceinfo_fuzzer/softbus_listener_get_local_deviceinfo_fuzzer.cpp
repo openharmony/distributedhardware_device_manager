3dd76a2a6e321cb89497499385cd7e61fa18615a
wuxiaodong <wuxiaodong21@huawei.com>
2024-04-16 17:52:25 +0800

    add fuzz
    Signed-off-by: wuxiaodong <wuxiaodong21@huawei.com>

--- test/softbusfuzztest/softbuslistenergetlocaldeviceinfo_fuzzer/softbus_listener_get_local_deviceinfo_fuzzer.cpp
+++ test/softbusfuzztest/softbuslistenergetlocaldeviceinfo_fuzzer/softbus_listener_get_local_deviceinfo_fuzzer.cpp
@@ -27,7 +27,7 @@ namespace OHOS {
 namespace DistributedHardware {
 void GetLocalDeviceInfoFuzzTest(const uint8_t* data, size_t size)
 {
-    if ((data == nullptr) || (size <= sizeof(int32_t))) {
+    if ((data == nullptr) || (size < sizeof(int32_t))) {
         return;
     }
 
