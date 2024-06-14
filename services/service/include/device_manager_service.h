1cb0eb352825b15bc16af07e6b6a45b695c2dc7b
q30043944 <qinlong18@huawei.com>
2024-06-14 17:52:00 +0800

    内存优化
    Signed-off-by: q30043944 <qinlong18@huawei.com>

--- services/service/include/device_manager_service.h
+++ services/service/include/device_manager_service.h
@@ -228,6 +228,7 @@ private:
     std::shared_ptr<PinHolder> pinHolder_;
 #if !(defined(__LITEOS_M__) || defined(LITE_DEVICE))
     std::shared_ptr<DmAccountCommonEventManager> accountCommonEventManager_;
+    bool isFirstUserSwitched_ = true;
 #if defined(SUPPORT_BLUETOOTH) || defined(SUPPORT_WIFI)
     std::shared_ptr<DmPublishCommonEventManager> publshCommonEventManager_;
 #endif // SUPPORT_BLUETOOTH  SUPPORT_WIFI
