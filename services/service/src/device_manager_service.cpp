db76273760e9ca77267515c625789e61b3ca1f7f
史晓晓 <shixiaoxiao.30044129@huawei.com>
2024-08-24 16:26:28 +0800

    修改编译问题
    Signed-off-by: 史晓晓 <shixiaoxiao.30044129@huawei.com>

--- services/service/src/device_manager_service.cpp
+++ services/service/src/device_manager_service.cpp
@@ -1618,16 +1618,16 @@ int32_t DeviceManagerService::SetDnPolicy(const std::string &pkgName, std::map<s
         LOGE("Invalid parameter, pkgName is empty.");
         return ERR_DM_INPUT_PARA_INVALID;
     }
-    int32_t policy = -1;
-    if (policy.find(DM_POLICY_STRATEGY_FOR_BLE) != policy.end()) {
-        policy = std::stoi((policy.find(DM_POLICY_STRATEGY_FOR_BLE)->second).c_str());
+    int32_t policyStrategy = -1;
+    if (policy.find(PARAM_KEY_POLICY_STRATEGY_FOR_BLE) != policy.end()) {
+        policyStrategy = std::stoi((policy.find(PARAM_KEY_POLICY_STRATEGY_FOR_BLE)->second).c_str());
     }
     uint8_t timeOut = 0;
-    if (policy.find(DM_POLICY_TIMEOUT) != policy.end()) {
-        timeOut = std::stoi((policy.find(DM_POLICY_TIMEOUT)->second).c_str());
+    if (policy.find(PARAM_KEY_POLICY_TIME_OUT) != policy.end()) {
+        timeOut = std::stoi((policy.find(PARAM_KEY_POLICY_TIME_OUT)->second).c_str());
     }
     CHECK_NULL_RETURN(softbusListener_, ERR_DM_POINT_NULL);
-    return softbusListener_->SetDnPolicy(pkgName, policy, timeOut);
+    return softbusListener_->SetDnPolicy(policyStrategy, timeOut);
 }
 } // namespace DistributedHardware
 } // namespace OHOS
