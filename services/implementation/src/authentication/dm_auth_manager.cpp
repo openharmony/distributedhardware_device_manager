c9449159392a98901af12f773fb9ad2dc25cfba3
gaoqiang_strong <gaoqiang64@huawei.com>
2024-09-24 20:59:22 +0800

    修改检视意见
    Signed-off-by: gaoqiang_strong <gaoqiang64@huawei.com>

--- services/implementation/src/authentication/dm_auth_manager.cpp
+++ services/implementation/src/authentication/dm_auth_manager.cpp
@@ -303,14 +303,11 @@ int32_t DmAuthManager::StopAuthenticateDevice(const std::string &pkgName)
         LOGE("Invalid parameter, pkgName is empty.");
         return ERR_DM_FAILED;
     }
-    if (authRequestState_!= nullptr || authResponseContext_ != nullptr) {
-        if (isAuthenticateDevice_) {
-            LOGI("Stop previous AuthenticateDevice.");
-            authRequestContext_->reason = STOP_BIND;
-            authResponseContext_->state = authRequestState_->GetStateType();
-            authRequestState_->TransitionTo(std::make_shared<AuthRequestFinishState>());
-            return DM_OK;
-        }
+    if ((authRequestState_!= nullptr || authResponseContext_ != nullptr) && isAuthenticateDevice_) {
+        LOGI("Stop previous AuthenticateDevice.");
+        authRequestContext_->reason = STOP_BIND;
+        authResponseContext_->state = authRequestState_->GetStateType();
+        authRequestState_->TransitionTo(std::make_shared<AuthRequestFinishState>());
     }
     return DM_OK;
 }
