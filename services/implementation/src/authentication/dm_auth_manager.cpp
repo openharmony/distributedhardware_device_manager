4a90f143e492671ee03c05b3f2782cc8ac94986b
q30043944 <qinlong18@huawei.com>
2024-09-26 09:54:19 +0800

    OObe异常校验修改
    Signed-off-by: q30043944 <qinlong18@huawei.com>

--- services/implementation/src/authentication/dm_auth_manager.cpp
+++ services/implementation/src/authentication/dm_auth_manager.cpp
@@ -824,6 +824,14 @@ void DmAuthManager::SendAuthRequest(const int32_t &sessionId)
 void DmAuthManager::ProcessAuthRequest(const int32_t &sessionId)
 {
     LOGI("ProcessAuthRequest start.");
+    if (authResponseContext_->authType == AUTH_TYPE_IMPORT_AUTH_CODE &&
+        !authResponseContext_->importAuthCode.empty() && !importAuthCode_.empty()) {
+        if (authResponseContext_->importAuthCode != Crypto::Sha256(importAuthCode_)) {
+            SetReasonAndFinish(ERR_DM_AUTH_CODE_INCORRECT, AuthState::AUTH_REQUEST_FINISH);
+            return;
+        }
+    }
+
     if (authResponseContext_->isOnline && softbusConnector_->CheckIsOnline(remoteDeviceId_)) {
         authResponseContext_->isOnline = true;
     } else {
@@ -2460,13 +2468,8 @@ void DmAuthManager::HandleSessionHeartbeat(std::string name)
 
 int32_t DmAuthManager::CheckTrustState()
 {
-    if (authResponseContext_->isOnline && authResponseContext_->authType == AUTH_TYPE_IMPORT_AUTH_CODE &&
-        !authResponseContext_->importAuthCode.empty() && !importAuthCode_.empty()) {
-        if (authResponseContext_->importAuthCode == Crypto::Sha256(importAuthCode_)) {
-            SetReasonAndFinish(DM_OK, AuthState::AUTH_REQUEST_FINISH);
-        } else {
-            SetReasonAndFinish(ERR_DM_AUTH_CODE_INCORRECT, AuthState::AUTH_REQUEST_FINISH);
-        }
+    if (authResponseContext_->isOnline && authResponseContext_->authType == AUTH_TYPE_IMPORT_AUTH_CODE) {
+        SetReasonAndFinish(DM_OK, AuthState::AUTH_REQUEST_FINISH);
         return ALREADY_BIND;
     }
     if (authResponseContext_->isIdenticalAccount) {
