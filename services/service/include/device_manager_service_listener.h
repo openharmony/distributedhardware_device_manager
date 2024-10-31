fa276112d5f2ec803922b20b5960fd7c5833dfa7
史晓晓 <shixiaoxiao.30044129@huawei.com>
2024-10-31 04:15:02 +0000

    update services/service/include/device_manager_service_listener.h.
    Signed-off-by: 史晓晓 <shixiaoxiao.30044129@huawei.com>

--- services/service/include/device_manager_service_listener.h
+++ services/service/include/device_manager_service_listener.h
@@ -74,8 +74,6 @@ public:
     void OnDeviceScreenStateChange(const std::string &pkgName, DmDeviceInfo &devInfo) override;
     void OnCredentialAuthStatus(const std::string &pkgName, const std::string &proofInfo, uint16_t deviceTypeId,
                                 int32_t errcode) override;
-
-    void OnDeviceScreenStateChange(const std::string &pkgName, DmDeviceInfo &devInfo) override;
 private:
     void ConvertDeviceInfoToDeviceBasicInfo(const std::string &pkgName,
         const DmDeviceInfo &info, DmDeviceBasicInfo &deviceBasicInfo);
