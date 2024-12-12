8a4cadc512d1551209688493a857ee3e844920e6
yangwei_814916 <yangwei271@huawei.com>
2024-12-12 21:51:49 +0800

    增加状态
    Signed-off-by: yangwei_814916 <yangwei271@huawei.com>

--- services/implementation/include/authentication/auth_request_state.h
+++ services/implementation/include/authentication/auth_request_state.h
@@ -193,6 +193,12 @@ public:
     int32_t GetStateType() override;
     int32_t Enter() override;
 };
+
+class AuthRequestAuthFinish : public AuthRequestState {
+public:
+    int32_t GetStateType() override;
+    int32_t Enter() override;
+};
 } // namespace DistributedHardware
 } // namespace OHOS
 #endif // OHOS_DM_AUTH_REQUEST_STATE_H
