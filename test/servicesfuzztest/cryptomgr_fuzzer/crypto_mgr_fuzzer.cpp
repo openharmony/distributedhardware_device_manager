b4f5dfd7ce3c18115345b07a96bb554153bc13e2
l60055366 <liuxiang188@h-partners.com>
2025-02-22 16:41:03 +0800

    新增dm模块的fuzz用例
    Signed-off-by: l60055366 <liuxiang188@h-partners.com>

--- test/servicesfuzztest/cryptomgr_fuzzer/crypto_mgr_fuzzer.cpp
+++ test/servicesfuzztest/cryptomgr_fuzzer/crypto_mgr_fuzzer.cpp
@@ -33,8 +33,8 @@ namespace DistributedHardware {
 namespace {
     constexpr uint32_t CIPHER_TEXT_LEN = 128;
     constexpr uint32_t PLAIN_TEXT_SIZE = 5;
-    constexpr uint32_t PLAIN_LEN = 10;
-    constexpr uint32_t CIPHER_TEXT_SIZE = 12;
+    constexpr uint32_t PLAIN_LEN = 50;
+    constexpr uint32_t CIPHER_TEXT_SIZE = 30;
     constexpr uint32_t KEY_LEN = 1024;
 }
 
