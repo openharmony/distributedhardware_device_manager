a4cbb74dcafb1feb7a35608920df95309ba3654f
l60055366 <liuxiang188@h-partners.com>
2025-03-15 16:09:51 +0800

    修改dm模块的用例级别
    Signed-off-by: l60055366 <liuxiang188@h-partners.com>

--- test/unittest/UTTest_device_name_manager.cpp
+++ test/unittest/UTTest_device_name_manager.cpp
@@ -74,14 +74,11 @@ HWTEST_F(DeviceNameManagerTest, Init_001, testing::ext::TestSize.Level1)
     EXPECT_CALL(*multipleUserConnector_, GetCurrentAccountUserID()).WillRepeatedly(Return(DEFAULT_VALUABLE_USER_ID));
     auto resultSet = std::make_shared<DataShareResultSetMock>(nullptr);
     EXPECT_CALL(*helper_, Query(_, _, _, _)).WillRepeatedly(Return(resultSet));
-    EXPECT_CALL(*helper_, RegisterObserver(_, _)).Times(AtLeast(1));
     EXPECT_CALL(*helper_, Release()).WillRepeatedly(Return(true));
 
     EXPECT_CALL(*resultSet, GetRowCount(_)).WillRepeatedly(DoAll(SetArgReferee<0>(1), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, GoToRow(_)).Times(AtLeast(1));
     EXPECT_CALL(*resultSet, GetString(_, _)).WillRepeatedly(
         DoAll(SetArgReferee<1>(subffixName), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, Close()).Times(AtLeast(1));
 
     DeviceNameManager::GetInstance().InitDeviceNameWhenSoftBusReady();
     DeviceNameManager::GetInstance().UnInit();
@@ -109,15 +106,11 @@ HWTEST_F(DeviceNameManagerTest, InitDeviceNameWhenUserSwitch_001, testing::ext::
     EXPECT_CALL(*multipleUserConnector_, GetCurrentAccountUserID()).WillRepeatedly(Return(DEFAULT_VALUABLE_USER_ID));
     auto resultSet = std::make_shared<DataShareResultSetMock>(nullptr);
     EXPECT_CALL(*helper_, Query(_, _, _, _)).WillRepeatedly(Return(resultSet));
-    EXPECT_CALL(*helper_, RegisterObserver(_, _)).Times(AtLeast(1));
-    EXPECT_CALL(*helper_, UnregisterObserver(_, _)).Times(AtLeast(1));
     EXPECT_CALL(*helper_, Release()).WillRepeatedly(Return(true));
 
     EXPECT_CALL(*resultSet, GetRowCount(_)).WillRepeatedly(DoAll(SetArgReferee<0>(1), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, GoToRow(_)).Times(AtLeast(1));
     EXPECT_CALL(*resultSet, GetString(_, _)).WillRepeatedly(
         DoAll(SetArgReferee<1>(subffixName), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, Close()).Times(AtLeast(1));
 
     DeviceNameManager::GetInstance().InitDeviceNameWhenSoftBusReady();
     DeviceNameManager::GetInstance().InitDeviceNameWhenUserSwitch(curUserId, preUserId);
@@ -148,10 +141,8 @@ HWTEST_F(DeviceNameManagerTest, InitDeviceNameWhenUserSwitch_002, testing::ext::
     EXPECT_CALL(*helper_, Release()).WillRepeatedly(Return(true));
 
     EXPECT_CALL(*resultSet, GetRowCount(_)).WillRepeatedly(DoAll(SetArgReferee<0>(1), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, GoToRow(_)).Times(AtLeast(1));
     EXPECT_CALL(*resultSet, GetString(_, _)).WillRepeatedly(
         DoAll(SetArgReferee<1>(subffixName), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, Close()).Times(AtLeast(1));
 
     DeviceNameManager::GetInstance().InitDeviceNameWhenUserSwitch(curUserId, preUserId);
     DeviceNameManager::GetInstance().UnInit();
@@ -179,11 +170,9 @@ HWTEST_F(DeviceNameManagerTest, InitDeviceNameWhenLogout_001, testing::ext::Test
     EXPECT_CALL(*helper_, Query(_, _, _, _)).WillRepeatedly(Return(resultSet));
     EXPECT_CALL(*helper_, Release()).WillRepeatedly(Return(true));
 
-    EXPECT_CALL(*resultSet, GetRowCount(_)).WillRepeatedly(DoAll(SetArgReferee<0>(1), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, GoToRow(_)).Times(AtLeast(1));
+    EXPECT_CALL(*resultSet, GetRowCount(_)).WillRepeatedly(DoAll(SetArgReferee<0>(1), Return(DataShare::E_OK)));    
     EXPECT_CALL(*resultSet, GetString(_, _)).WillRepeatedly(
         DoAll(SetArgReferee<1>(subffixName), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, Close()).Times(AtLeast(1));
 
     DeviceNameManager::GetInstance().InitDeviceNameWhenLogout();
     DeviceNameManager::GetInstance().UnInit();
@@ -244,10 +233,8 @@ HWTEST_F(DeviceNameManagerTest, InitDeviceNameWhenNickChange_001, testing::ext::
     EXPECT_CALL(*helper_, Release()).WillRepeatedly(Return(true));
 
     EXPECT_CALL(*resultSet, GetRowCount(_)).WillRepeatedly(DoAll(SetArgReferee<0>(1), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, GoToRow(_)).Times(AtLeast(1));
     EXPECT_CALL(*resultSet, GetString(_, _)).WillRepeatedly(
         DoAll(SetArgReferee<1>(subffixName), Return(DataShare::E_OK)));
-    EXPECT_CALL(*resultSet, Close()).Times(AtLeast(1));
 
     DeviceNameManager::GetInstance().InitDeviceNameWhenNickChange();
     DeviceNameManager::GetInstance().UnInit();
