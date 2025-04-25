152d0d2526409e04cd712ea5a96619b69f1020aa
BrainL <libuyan1@huawei.com>
2025-04-25 15:12:51 +0800

    modify UT
    Signed-off-by: BrainL <libuyan1@huawei.com>

--- test/commonunittest/UTTest_dm_deviceprofile_connector.cpp
+++ test/commonunittest/UTTest_dm_deviceprofile_connector.cpp
@@ -1124,7 +1124,7 @@ HWTEST_F(DeviceProfileConnectorTest, GetAuthForm_001, testing::ext::TestSize.Lev
     profile.SetBindType(DM_IDENTICAL_ACCOUNT);
     ret = DeviceProfileConnector::GetInstance().GetAuthForm(profile, trustDev, reqDev);
     EXPECT_EQ(ret, IDENTICAL_ACCOUNT_TYPE);
-    profile.SetBindType(DM_SHARE_TYPE);
+    profile.SetBindType(SHARE_TYPE);
     profile.SetBindLevel(DEVICE);
     ret = DeviceProfileConnector::GetInstance().GetAuthForm(profile, trustDev, reqDev);
     EXPECT_EQ(ret, SHARE_TYPE);
