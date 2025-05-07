a4a1165d6adddae073433fd0d14cd74c0c2ff322
李巍 <liwei787@huawei.com>
2025-05-07 22:15:33 +0800

    add log
    Signed-off-by: 李巍 <liwei787@huawei.com>

--- commondependency/src/deviceprofile_connector.cpp
+++ commondependency/src/deviceprofile_connector.cpp
@@ -2543,7 +2543,7 @@ int32_t DeviceProfileConnector::UpdateLocalServiceInfo(
 int32_t DeviceProfileConnector::GetLocalServiceInfoByBundleNameAndPinExchangeType(const std::string &bundleName,
     int32_t pinExchangeType, DistributedDeviceProfile::LocalServiceInfo &localServiceInfo)
 {
-    LOGI("getLocal bundleName = %{public}s, pinExchangeType = %{public}d", bundleName.c_str(), pinExchangeType);
+    LOGI("getLocalServiceInfo bundleName = %{public}s, pinExchangeType = %{public}d", bundleName.c_str(), pinExchangeType);
     int32_t ret = DistributedDeviceProfileClient::GetInstance().GetLocalServiceInfoByBundleAndPinType(bundleName,
         pinExchangeType, localServiceInfo);
     if (ret != DM_OK) {
