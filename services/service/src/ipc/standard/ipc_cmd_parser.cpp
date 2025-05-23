17bdb7cc041b194ae1073bbc2ae2a310288e3276
yangwei_814916 <yangwei271@huawei.com>
2025-05-23 20:23:16 +0800

    ACL校验
    Signed-off-by: yangwei_814916 <yangwei271@huawei.com>

--- services/service/src/ipc/standard/ipc_cmd_parser.cpp
+++ services/service/src/ipc/standard/ipc_cmd_parser.cpp
@@ -68,6 +68,7 @@ void DecodeDmAccessCallee(MessageParcel &parcel, DmAccessCallee &callee)
     callee.accountId = parcel.ReadString();
     callee.networkId = parcel.ReadString();
     callee.peerId = parcel.ReadString();
+    callee.pkgName = parcel.ReadString();
     callee.userId = parcel.ReadInt32();
     callee.extra = parcel.ReadString();
     callee.tokenId = parcel.ReadUint64();
