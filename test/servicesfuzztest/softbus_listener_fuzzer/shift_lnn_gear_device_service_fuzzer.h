87f9b8c160cd3e20a62bc5c1fe2170d7644ea4b3
李尚 <lishang21@huawei.com>
2025-05-27 13:37:32 +0000

    1
    Signed-off-by: 李尚 <lishang21@huawei.com>

--- test/servicesfuzztest/softbus_listener_fuzzer/shift_lnn_gear_device_service_fuzzer.h
+++ test/servicesfuzztest/softbus_listener_fuzzer/shift_lnn_gear_device_service_fuzzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
-#ifndef SHIFT_LNN_GEAR_DEVICE_SERVICE_FUZZER_H
-#define SHIFT_LNN_GEAR_DEVICE_SERVICE_FUZZER_H
+#ifndef DM_SOFTBUS_LISTENER_FUZZER_H
+#define DM_SOFTBUS_LISTENER_FUZZER_H
 
-#define FUZZ_PROJECT_NAME "shiftlnngeardeviceservice_fuzzer"
+#define FUZZ_PROJECT_NAME "dm_softbus_listener_fuzzer"
 
-#endif // SHIFT_LNN_GEAR_DEVICE_SERVICE_FUZZER_H
+#endif // DM_SOFTBUS_LISTENER_FUZZER_H
