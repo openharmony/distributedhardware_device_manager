1cdde2a550173ea017162feb6ed450699d5d0fb2
史晓晓 <shixiaoxiao.30044129@huawei.com>
2025-07-15 10:34:43 +0800

    修改
    Signed-off-by: 史晓晓 <shixiaoxiao.30044129@huawei.com>

--- services/service/src/permission/standard/permission_manager.cpp
+++ services/service/src/permission/standard/permission_manager.cpp
@@ -33,8 +33,6 @@ constexpr const char* DM_SERVICE_ACCESS_PERMISSION = "ohos.permission.ACCESS_SER
 constexpr const char* DM_SERVICE_ACCESS_NEWPERMISSION = "ohos.permission.DISTRIBUTED_DATASYNC";
 constexpr const char* DM_MONITOR_DEVICE_NETWORK_STATE_PERMISSION = "ohos.permission.MONITOR_DEVICE_NETWORK_STATE";
 constexpr const static char* AUTH_CODE_WHITE_LIST[] = {
-    "com.huawei.msdp.hmringgenerator",
-    "com.huawei.msdp.hmringdiscriminator",
     "CollaborationFwk",
     "wear_link_service",
     "watch_system_service",
@@ -44,13 +42,12 @@ constexpr const static char* AUTH_CODE_WHITE_LIST[] = {
 };
 constexpr int32_t AUTH_CODE_WHITE_LIST_NUM = std::size(AUTH_CODE_WHITE_LIST);
 
-constexpr int32_t PIN_HOLDER_WHITE_LIST_NUM = 1;
-constexpr const static char* g_pinHolderWhiteList[PIN_HOLDER_WHITE_LIST_NUM] = {
+constexpr const static char* PIN_HOLDER_WHITE_LIST[] = {
     "CollaborationFwk",
 };
+constexpr int32_t PIN_HOLDER_WHITE_LIST_NUM = std::size(PIN_HOLDER_WHITE_LIST);
 
-constexpr int32_t SYSTEM_SA_WHITE_LIST_NUM = 10;
-constexpr const static char* SYSTEM_SA_WHITE_LIST[SYSTEM_SA_WHITE_LIST_NUM] = {
+constexpr const static char* SYSTEM_SA_WHITE_LIST[] = {
     "Samgr_Networking",
     "ohos.distributeddata.service",
     "ohos.dslm",
@@ -62,37 +59,40 @@ constexpr const static char* SYSTEM_SA_WHITE_LIST[SYSTEM_SA_WHITE_LIST_NUM] = {
     "audio_manager_service",
     "hmos.collaborationfwk.deviceDetect",
 };
+constexpr int32_t SYSTEM_SA_WHITE_LIST_NUM = std::size(SYSTEM_SA_WHITE_LIST);
 
-constexpr const static char* SET_DN_POLICY_WHITE_LIST[] = {
+constexpr const static char* SETDNPOLICY_WHITE_LIST[] = {
     "collaboration_service",
     "watch_system_service",
     "com.huawei.hmos.walletservice",
     "com.ohos.distributedjstest",
     "glasses_collaboration_service",
 };
-constexpr uint32_t SETDNPOLICY_WHITE_LIST_NUM = std::size(SET_DN_POLICY_WHITE_LIST);
+constexpr uint32_t SETDNPOLICY_WHITE_LIST_NUM = std::size(SETDNPOLICY_WHITE_LIST);
 
-constexpr uint32_t GETDEVICEINFO_WHITE_LIST_NUM = 3;
-constexpr const static char* g_getDeviceInfoWhiteList[GETDEVICEINFO_WHITE_LIST_NUM] = {
+constexpr const static char* GETDEVICEINFO_WHITE_LIST[] = {
     "gameservice_server",
     "com.huawei.hmos.slassistant",
     "token_sync_service",
 };
-constexpr int32_t MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST_NUM = 2;
-constexpr const static char* g_modifyLocalDeviceNameWhiteList[MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST_NUM] = {
+constexpr uint32_t GETDEVICEINFO_WHITE_LIST_NUM = std::size(GETDEVICEINFO_WHITE_LIST);
+
+constexpr const static char* MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST[] = {
     "com.huawei.hmos.settings",
     "com.huawei.hmos.tvcooperation",
 };
-constexpr int32_t MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST_NUM = 1;
-constexpr const static char* g_modifyRemoteDeviceNameWhiteList[MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST_NUM] = {
+constexpr int32_t MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST_NUM = std::size(MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST);
+
+constexpr const static char* MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST[] = {
     "com.ohos.settings",
 };
+constexpr int32_t MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST_NUM = std::size(MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST);
 
-constexpr int32_t PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST_NUM = 2;
-constexpr const static char* g_putDeviceProfileInfoListWhiteList[PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST_NUM] = {
+constexpr const static char* PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST[] = {
     "com.huawei.hmos.ailifesvc",
     "com.huawei.hmos.tvcooperation",
 };
+constexpr int32_t PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST_NUM = std::size(PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST);
 }
 
 bool PermissionManager::CheckPermission(void)
@@ -211,7 +211,7 @@ bool PermissionManager::CheckProcessNameValidOnPinHolder(const std::string &proc
 
     uint16_t index = 0;
     for (; index < PIN_HOLDER_WHITE_LIST_NUM; ++index) {
-        std::string tmp(g_pinHolderWhiteList[index]);
+        std::string tmp(PIN_HOLDER_WHITE_LIST[index]);
         if (processName == tmp) {
             return true;
         }
@@ -262,7 +262,7 @@ bool PermissionManager::CheckProcessNameValidOnSetDnPolicy(const std::string &pr
     }
     uint16_t index = 0;
     for (; index < SETDNPOLICY_WHITE_LIST_NUM; ++index) {
-        std::string tmp(SET_DN_POLICY_WHITE_LIST[index]);
+        std::string tmp(SETDNPOLICY_WHITE_LIST[index]);
         if (processName == tmp) {
             return true;
         }
@@ -278,7 +278,7 @@ bool PermissionManager::CheckProcessNameValidOnGetDeviceInfo(const std::string &
     }
     uint16_t index = 0;
     for (; index < GETDEVICEINFO_WHITE_LIST_NUM; ++index) {
-        std::string tmp(g_getDeviceInfoWhiteList[index]);
+        std::string tmp(GETDEVICEINFO_WHITE_LIST[index]);
         if (processName == tmp) {
             return true;
         }
@@ -294,7 +294,7 @@ bool PermissionManager::CheckProcessNameValidModifyLocalDeviceName(const std::st
     }
     uint16_t index = 0;
     for (; index < MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST_NUM; ++index) {
-        std::string tmp(g_modifyLocalDeviceNameWhiteList[index]);
+        std::string tmp(MODIFY_LOCAL_DEVICE_NAME_WHITE_LIST[index]);
         if (processName == tmp) {
             return true;
         }
@@ -310,7 +310,7 @@ bool PermissionManager::CheckProcessNameValidModifyRemoteDeviceName(const std::s
     }
     uint16_t index = 0;
     for (; index < MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST_NUM; ++index) {
-        std::string tmp(g_modifyRemoteDeviceNameWhiteList[index]);
+        std::string tmp(MODIFY_REMOTE_DEVICE_NAME_WHITE_LIST[index]);
         if (processName == tmp) {
             return true;
         }
@@ -326,7 +326,7 @@ bool PermissionManager::CheckProcessNameValidPutDeviceProfileInfoList(const std:
     }
     uint16_t index = 0;
     for (; index < PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST_NUM; ++index) {
-        std::string tmp(g_putDeviceProfileInfoListWhiteList[index]);
+        std::string tmp(PUT_DEVICE_PROFILE_INFO_LIST_WHITE_LIST[index]);
         if (processName == tmp) {
             return true;
         }
