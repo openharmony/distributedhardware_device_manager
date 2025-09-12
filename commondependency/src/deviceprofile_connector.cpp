029cf8a3834a3d7f8df3ac559edd2e7e9ad5bf1c
李尚 <lishang21@h-partners.com>
2025-09-12 11:19:19 +0800

    dm
    Signed-off-by: 李尚 <lishang21@h-partners.com>

--- commondependency/src/deviceprofile_connector.cpp
+++ commondependency/src/deviceprofile_connector.cpp
@@ -1830,7 +1830,7 @@ DM_EXPORT bool DeviceProfileConnector::CheckAccessControl(
     const DmAccessCaller &caller, const std::string &srcUdid,
     const DmAccessCallee &callee, const std::string &sinkUdid)
 {
-    LOGI("srcUdid %{public}s, srcUserId %{public}d, srcPkgName %{public}s srcAccountId %{public}s,"
+    LOGI("srcUdid %{public}s, srcUserId %{public}d, srcPkgName %{public}s, srcAccountId %{public}s,"
         "sinkUdid %{public}s, sinkUserId %{public}d, sinkPkgName %{public}s, sinkAccountId %{public}s"
         "callerTokenId %{public}s, calleeTokenId %{public}s.", GetAnonyString(srcUdid).c_str(), caller.userId,
         caller.pkgName.c_str(), GetAnonyString(caller.accountId).c_str(),
