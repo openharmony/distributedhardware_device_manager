1760647cd0ec38d62923320e11f7b8fd02350a67
wzh213 <wangzhaohao@h-partners.com>
2025-09-18 11:00:10 +0800

    dm fuzz整改
    Signed-off-by: wzh213 <wangzhaohao@h-partners.com>

--- test/commonfuzztest/pinauthui_fuzzer/pinauthui_fuzzer.cpp
+++ test/commonfuzztest/pinauthui_fuzzer/pinauthui_fuzzer.cpp
@@ -13,7 +13,6 @@
  * limitations under the License.
  */
 
-#include <fuzzer/FuzzedDataProvider.h>
 #include "pin_auth_ui.h"
 
 #include "dm_ability_manager.h"
@@ -32,18 +31,11 @@ void PinAuthUiFuzzTest(const uint8_t* data, size_t size)
     if ((data == nullptr) || (size < sizeof(int32_t))) {
         return;
     }
-    FuzzedDataProvider fdp(data, size);
-    int32_t pageId = fdp.ConsumeIntegral<int32_t>();
-    std::shared_ptr<SoftbusConnector> softbusConnector = std::make_shared<SoftbusConnector>();
-    std::shared_ptr<DeviceManagerServiceListener> listener = std::make_shared<DeviceManagerServiceListener>();
-    std::shared_ptr<HiChainConnector> hiChainConnector = std::make_shared<HiChainConnector>();
-    std::shared_ptr<HiChainAuthConnector> hiChainAuthConnector = std::make_shared<HiChainAuthConnector>();
-    std::shared_ptr<DmAuthManager> authManager =
-        std::make_shared<DmAuthManager>(softbusConnector, hiChainConnector, listener, hiChainAuthConnector);
-    authManager->remoteDeviceId_ = fdp.ConsumeRandomLengthString();
+
+    int32_t pageId = *(reinterpret_cast<const int32_t*>(data));
+    std::shared_ptr<DmAuthManager> authManager = nullptr;
     std::shared_ptr<PinAuthUi> pinauthui = std::make_shared<PinAuthUi>();
     pinauthui->ShowPinDialog(pageId, authManager);
-    pinauthui->InputPinDialog(authManager);
 }
 }
 }
