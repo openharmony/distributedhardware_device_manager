bd70f59994ebcf52c0204e169ac90b10269bbd1f
Summer_58 <wangchangsheng9@h-partners.com>
2025-12-06 18:36:23 +0800

    add UT
    Signed-off-by: Summer_58 <wangchangsheng9@h-partners.com>

--- test/unittest/mock/device_manager_service_impl_mock.h
+++ test/unittest/mock/device_manager_service_impl_mock.h
@@ -53,6 +53,11 @@ public:
         const DmAccessCallee &callee, const std::string &sinkUdid) = 0;
     virtual int32_t DeleteAclExtraDataServiceId(int64_t serviceId, int64_t tokenIdCaller,
         std::string &udid, int32_t &bindLevel) = 0;
+    virtual int32_t SyncLocalAclListProcess(const DevUserInfo &localDevUserInfo,
+        const DevUserInfo &remoteDevUserInfo, std::string remoteAclList, bool isDelImmediately) = 0;
+    virtual void HandleDeviceUnBind(int32_t bindType, const std::string &peerUdid,
+        const std::string &localUdid, int32_t localUserId, const std::string &localAccountId) = 0;
+
 public:
     static inline std::shared_ptr<DmDeviceManagerServiceImpl> dmDeviceManagerServiceImpl = nullptr;
 };
@@ -81,6 +86,10 @@ public:
         const DmAccessCallee &, const std::string &));
     MOCK_METHOD(int32_t, DeleteAclExtraDataServiceId, (int64_t, int64_t,
         std::string &, int32_t &), (override));
+    MOCK_METHOD(int32_t, SyncLocalAclListProcess, (const DevUserInfo &localDevUserInfo,
+        const DevUserInfo &remoteDevUserInfo, std::string remoteAclList, bool isDelImmediately));
+    MOCK_METHOD(void, HandleDeviceUnBind, (int32_t, const std::string &,
+        const std::string &, int32_t, const std::string &));
 };
 }
 }
