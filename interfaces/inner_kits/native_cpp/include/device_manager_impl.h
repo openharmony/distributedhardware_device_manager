ee076c2e500ea0bfc298839c1fd1766460222c0c
zhaochaoyan <zhaochaoyan1@h-partners.com>
2026-02-10 19:09:06 +0800

    update: 更新文件 device_manager_impl.h
    Signed-off-by: zhaochaoyan <zhaochaoyan1@h-partners.com>

--- interfaces/inner_kits/native_cpp/include/device_manager_impl.h
+++ interfaces/inner_kits/native_cpp/include/device_manager_impl.h
@@ -440,8 +440,8 @@ public:
     virtual bool CheckSrcIsSameAccount(const DmAccessCaller &caller, const DmAccessCallee &callee) override;
     virtual bool CheckSinkIsSameAccount(const DmAccessCaller &caller, const DmAccessCallee &callee) override;
     virtual int32_t GetIdentificationByDeviceIds(const std::string &pkgName,
- 	        const std::vector<std::string> deviceIdList,
- 	        std::map<std::string, std::string> &deviceIdentificationMap) override;
+ 	    const std::vector<std::string> deviceIdList,
+ 	    std::map<std::string, std::string> &deviceIdentificationMap) override;
     virtual int32_t BindServiceTarget(const std::string &pkgName, const PeerTargetId &targetId,
         std::map<std::string, std::string> &bindParam, std::shared_ptr<BindTargetCallback> callback) override;
     virtual int32_t UnbindServiceTarget(const std::string &pkgName, int64_t serviceId) override;
