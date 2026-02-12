728c47985ab8c2d4b7a3a63981d36ad6f0480a3d
zhaochaoyan <zhaochaoyan1@h-partners.com>
2026-02-12 17:24:58 +0800

    update: 更新文件 permission_manager.cpp
    Signed-off-by: zhaochaoyan <zhaochaoyan1@h-partners.com>

--- services/service/src/permission/lite/permission_manager.cpp
+++ services/service/src/permission/lite/permission_manager.cpp
@@ -76,31 +76,25 @@ bool PermissionManager::CheckSystemSA(const std::string &pkgName)
     return true;
 }
 
-bool PermissionManager::CheckProcessNameValidOnSetDnPolicy(const std::string &processName)
-{
-    (void)processName;
-    return true;
-}
-
-bool PermissionManager::CheckProcessNameValidOnSetDnPolicy(const std::string &processName)
-{
-    (void)processName;
-    return true;
-}
-
-bool PermissionManager::CheckProcessNameValidOnGetDeviceInfo(const std::string &processName)
-{
-    (void)processName;
-    return true;
-}
-
 bool PermissionManager::CheckProcessNameValidModifyLocalDeviceName(const std::string &processName)
 {
     (void)processName;
     return true;
 }
 
+bool PermissionManager::CheckProcessNameValidOnSetDnPolicy(const std::string &processName)
+{
+    (void)processName;
+    return true;
+}
+
 bool PermissionManager::CheckProcessNameValidModifyRemoteDeviceName(const std::string &processName)
+{
+    (void)processName;
+    return true;
+}
+
+bool PermissionManager::CheckProcessNameValidOnGetDeviceInfo(const std::string &processName)
 {
     (void)processName;
     return true;
