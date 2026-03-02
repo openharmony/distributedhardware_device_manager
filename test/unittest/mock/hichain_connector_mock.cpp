a04542e395033c7fc2a4ca7d633842008e955de4
m0_64071286 <pankaiyuan4@h-partners.com>
2026-03-02 20:12:45 +0800

    update: 更新文件 hichain_connector_mock.cpp
    Signed-off-by: m0_64071286 <pankaiyuan4@h-partners.com>

--- test/unittest/mock/hichain_connector_mock.cpp
+++ test/unittest/mock/hichain_connector_mock.cpp
@@ -19,47 +19,142 @@
 
 namespace OHOS {
 namespace DistributedHardware {
+namespace {
+constexpr int32_t MOCK_CONNECTOR_NOT_READY = -1;
+}
+
 bool HiChainConnector::GetGroupInfo(const int32_t userId, const std::string &queryParams,
     std::vector<GroupInfo> &groupList)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return false;
+    }
     return DmHiChainConnector::dmHiChainConnector->GetGroupInfo(userId, queryParams, groupList);
 }
 
 bool HiChainConnector::IsDevicesInP2PGroup(const std::string &hostDevice, const std::string &peerDevice)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return false;
+    }
     return DmHiChainConnector::dmHiChainConnector->IsDevicesInP2PGroup(hostDevice, peerDevice);
 }
 
 int32_t HiChainConnector::GetRelatedGroups(const std::string &deviceId, std::vector<GroupInfo> &groupList)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
     return DmHiChainConnector::dmHiChainConnector->GetRelatedGroups(deviceId, groupList);
 }
 
 int32_t HiChainConnector::GetRelatedGroups(int32_t userId, const std::string &deviceId,
     std::vector<GroupInfo> &groupList)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
     return DmHiChainConnector::dmHiChainConnector->GetRelatedGroups(userId, deviceId, groupList);
 }
 
 int32_t HiChainConnector::DeleteGroupByACL(std::vector<std::pair<int32_t, std::string>> &delACLInfoVec,
     std::vector<int32_t> &userIdVec)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
     return DmHiChainConnector::dmHiChainConnector->DeleteGroupByACL(delACLInfoVec, userIdVec);
 }
 
 int32_t HiChainConnector::DeleteTimeOutGroup(const std::string &peerUdid, int32_t userId)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
     return DmHiChainConnector::dmHiChainConnector->DeleteTimeOutGroup(peerUdid, userId);
 }
 
 int32_t HiChainConnector::GetRelatedGroupsExt(const std::string &deviceId, std::vector<GroupInfo> &groupList)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
     return DmHiChainConnector::dmHiChainConnector->GetRelatedGroupsExt(deviceId, groupList);
 }
 
 int32_t HiChainConnector::DeleteGroupExt(int32_t userId, const std::string &groupId)
 {
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
     return DmHiChainConnector::dmHiChainConnector->DeleteGroupExt(userId, groupId);
+}
+
+int32_t HiChainConnector::getRegisterInfo(const std::string &queryParams, std::string &returnJsonStr)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->GetRegisterInfo(queryParams, returnJsonStr);
+}
+
+int32_t HiChainConnector::CreateGroup(int64_t requestId, int32_t authType, const std::string &userId,
+    JsonObject &jsonOutObj)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->CreateGroup(requestId, authType, userId, jsonOutObj);
+}
+
+int32_t HiChainConnector::addMultiMembers(const int32_t groupType, const std::string &userId,
+    const JsonObject &jsonDeviceList)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->AddMultiMembers(groupType, userId, jsonDeviceList);
+}
+
+int32_t HiChainConnector::addMultiMembersExt(const std::string &credentialInfo)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->AddMultiMembersExt(credentialInfo);
+}
+
+int32_t HiChainConnector::deleteMultiMembers(const int32_t groupType, const std::string &userId,
+    const JsonObject &jsonDeviceList)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->DeleteMultiMembers(groupType, userId, jsonDeviceList);
+}
+
+int32_t HiChainConnector::DeleteGroup(int64_t requestId, const std::string &userId, const int32_t authType)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->DeleteGroupByReqId(requestId, userId, authType);
+}
+
+int32_t HiChainConnector::RegisterHiChainGroupCallback(const std::shared_ptr<IDmGroupResCallback> &callback)
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->RegisterHiChainGroupCallback(callback);
+}
+
+int32_t HiChainConnector::UnRegisterHiChainGroupCallback()
+{
+    if (DmHiChainConnector::dmHiChainConnector == nullptr) {
+        return MOCK_CONNECTOR_NOT_READY;
+    }
+    return DmHiChainConnector::dmHiChainConnector->UnRegisterHiChainGroupCallback();
 }
 } // namespace DistributedHardware
 } // namespace OHOS
