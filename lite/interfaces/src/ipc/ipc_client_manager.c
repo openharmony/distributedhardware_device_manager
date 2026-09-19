bfc3fc025bb472e46f7d4283ba52ab5a969ff865
lw <liwei787@huawei.com>
2026-09-19 19:11:43 +0800

    feat(lite/interfaces): 新增 lite/interfaces/src 实现 + BUILD.gn
    - device_manager_impl.c(649): lite 设备管理实现
    - ipc/ipc_client_manager.c / ipc_client_stub.c / ipc_client_server_proxy.c / ipc_cmd_parser.c
    - notify/device_manager_notify.c
    - device_manager.c
    - BUILD.gn
    
    依赖: PR-1, PR-6 (split-pr-6-lite-json-iface-inc, 含 interfaces/include)
    
    Signed-off-by: lw <liwei787@huawei.com>
    Change-Id: I7223f1b45b84cb006c1e171e0872928ba6a7c99b

---
+++ lite/interfaces/src/ipc/ipc_client_manager.c
@@ -0,0 +1,222 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "ipc_client_manager.h"
+#include "ipc_client_server_proxy.h"
+#include "ipc_client_stub.h"
+#include "dm_log.h"
+#include "dm_error_type.h"
+#include "dm_container.h"
+#include "dm_thread.h"
+#include "dm_constants.h"
+#include "dm_device_info_c.h"
+#include "device_manager_ipc_interface_code.h"
+#include "serializer.h"
+#include "ipc_skeleton.h"
+#include "samgr_lite.h"
+#include "iunknown.h"
+#include "iproxy_client.h"
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "securec.h"
+
+#define DM_SAMGR_SERVICE_NAME "dev_mgr_svc"
+#define DM_MAX_IPC_DATA_LEN 2048
+
+typedef struct {
+    IpcIo* reply;
+} DmIpcOwner;
+
+static IpcClientManager g_ipcClientManager;
+static bool g_isInited = false;
+
+static int DmIpcNotify(IOwner owner, int code, IpcIo* reply)
+{
+    (void)code;
+    if (owner == NULL || reply == NULL) {
+        return EC_SUCCESS;
+    }
+    DmIpcOwner* data = (DmIpcOwner*)owner;
+    if (data->reply == NULL || data->reply->bufferBase == NULL) {
+        return EC_SUCCESS;
+    }
+    size_t dataLen = reply->bufferLeft;
+    size_t destCap = (size_t)(data->reply->bufferCur - data->reply->bufferBase) + data->reply->bufferLeft;
+    if (dataLen > destCap) {
+        dataLen = destCap;
+    }
+    if (dataLen > 0) {
+        (void)memcpy_s(data->reply->bufferBase, destCap, reply->bufferBase, dataLen);
+    }
+    data->reply->bufferCur = data->reply->bufferBase;
+    data->reply->bufferLeft = dataLen;
+    return EC_SUCCESS;
+}
+
+static int32_t DmIpcClientInvoke(uint32_t code, IpcIo* req, IpcIo* reply)
+{
+    IUnknown* iUnknown = SAMGR_GetInstance()->GetDefaultFeatureApi(DM_SAMGR_SERVICE_NAME);
+    if (iUnknown == NULL) {
+        LOGE("GetDefaultFeatureApi failed");
+        return ERR_DM_FAILED;
+    }
+    IClientProxy* proxy = (IClientProxy*)iUnknown;
+    if (proxy->Invoke == NULL) {
+        LOGE("Invoke is NULL");
+        return ERR_DM_FAILED;
+    }
+    DmIpcOwner owner = { .reply = reply };
+    return proxy->Invoke(proxy, (int)code, req, (IOwner)&owner, DmIpcNotify);
+}
+
+DM_EXPORT IpcClientManager* DmIpcClientManagerCreate(void)
+{
+    LOGI("create");
+    return &g_ipcClientManager;
+}
+
+DM_EXPORT void DmIpcClientManagerDestroy(IpcClientManager* self)
+{
+    (void)self;
+    g_isInited = false;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerInit(IpcClientManager* self, const DmString* pkgName)
+{
+    (void)self;
+    if (pkgName == NULL) {
+        return ERR_DM_INPUT_PARA_INVALID;
+    }
+    LOGI("init pkgName=%s", DmStringCstr(pkgName));
+    g_isInited = true;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerUnInit(IpcClientManager* self, const DmString* pkgName)
+{
+    (void)self;
+    (void)pkgName;
+    LOGI("un_init");
+    g_isInited = false;
+    return DM_OK;
+}
+
+DM_EXPORT void DmIpcClientManagerDestroyInner(IpcClientManager* self)
+{
+    (void)self;
+    g_isInited = false;
+}
+
+
+DM_EXPORT int32_t DmIpcClientManagerSendRequest(IpcClientManager* self, int32_t cmdCode, void* req, void* rsp)
+{
+    (void)self;
+    if (!g_isInited) {
+        LOGE("not inited");
+        return ERR_DM_FAILED;
+    }
+    IpcIo* reqIo = (IpcIo*)req;
+    IpcIo* rspIo = (IpcIo*)rsp;
+    if (reqIo == NULL || rspIo == NULL) {
+        return ERR_DM_INPUT_PARA_INVALID;
+    }
+    int32_t ret = DmIpcClientInvoke((uint32_t)cmdCode, reqIo, rspIo);
+    if (ret != 0) {
+        LOGE("invoke cmd %d failed %d", cmdCode, ret);
+        return ERR_DM_FAILED;
+    }
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerOnDmServiceDied(IpcClientManager* self)
+{
+    (void)self;
+    LOGI("service died");
+    g_isInited = false;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerConnectSa(IpcClientManager* self)
+{
+    (void)self;
+    IUnknown* iUnknown = SAMGR_GetInstance()->GetDefaultFeatureApi(DM_SAMGR_SERVICE_NAME);
+    if (iUnknown == NULL) {
+        LOGE("connect_sa failed");
+        return ERR_DM_FAILED;
+    }
+    return DM_OK;
+}
+
+DM_EXPORT void DmIpcClientManagerSubscribeSaChangeListener(IpcClientManager* self)
+{
+    (void)self;
+}
+
+DM_EXPORT void DmIpcClientManagerUnSubscribeSaChangeListener(IpcClientManager* self)
+{
+    (void)self;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerSubscribeSystemAbility(IpcClientManager* self)
+{
+    (void)self;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerUnsubscribeSystemAbility(IpcClientManager* self)
+{
+    (void)self;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerRegisterListener(IpcClientManager* self, const DmString* pkgName,
+    IpcClientStub* listener)
+{
+    (void)self;
+    (void)pkgName;
+    (void)listener;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerUnregisterListener(IpcClientManager* self, const DmString* pkgName)
+{
+    (void)self;
+    (void)pkgName;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerGetSaProxy(IpcClientManager* self)
+{
+    (void)self;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerSendRegisterListenerCmd(IpcClientManager* self, const DmString* pkgName,
+    IpcClientStub* listener)
+{
+    (void)self;
+    (void)pkgName;
+    (void)listener;
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientManagerSendUnregisterListenerCmd(IpcClientManager* self, const DmString* pkgName)
+{
+    (void)self;
+    (void)pkgName;
+    return DM_OK;
+}
---
+++ lite/interfaces/src/ipc/ipc_client_stub.c
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "ipc_client_stub.h"
+#include "dm_log.h"
+#include "dm_error_type.h"
+#include "dm_container.h"
+#include "dm_device_info_c.h"
+#include "device_manager_ipc_interface_code.h"
+#include "serializer.h"
+#include "ipc_skeleton.h"
+#include "device_manager_notify.h"
+#include <stdlib.h>
+#include "securec.h"
+
+DM_EXPORT IpcClientStub* DmIpcClientStubCreate(void)
+{
+    IpcClientStub* stub = (IpcClientStub*)calloc(1, sizeof(IpcClientStub));
+    return stub;
+}
+
+DM_EXPORT void DmIpcClientStubDestroy(IpcClientStub* self)
+{
+    if (self != NULL) {
+        free(self);
+    }
+}
+
+DM_EXPORT void DmIpcClientStubInit(IpcClientStub* self)
+{
+    (void)self;
+}
+
+DM_EXPORT void DmIpcClientStubDestroyInner(IpcClientStub* self)
+{
+    (void)self;
+}
+
+static int32_t DmIpcClientStubHandleDeviceState(IpcIo* data)
+{
+    int32_t state = 0;
+    int32_t isOnline = 0;
+    ReadInt32(data, &state);
+    ReadInt32(data, &isOnline);
+    size_t readLen = 0;
+    const char* networkId = (const char*)ReadString(data, &readLen);
+    const char* deviceName = (const char*)ReadString(data, &readLen);
+    int32_t deviceTypeId = 0;
+    ReadInt32(data, &deviceTypeId);
+    const char* deviceId = (const char*)ReadString(data, &readLen);
+    DmDeviceInfo deviceInfo;
+    (void)memset_s(&deviceInfo, sizeof(DmDeviceInfo), 0, sizeof(DmDeviceInfo));
+    if (networkId != NULL) {
+        (void)strncpy_s(deviceInfo.networkId, sizeof(deviceInfo.networkId), networkId,
+            sizeof(deviceInfo.networkId) - 1);
+    }
+    if (deviceName != NULL) {
+        (void)strncpy_s(deviceInfo.deviceName, sizeof(deviceInfo.deviceName), deviceName,
+            sizeof(deviceInfo.deviceName) - 1);
+    }
+    if (deviceId != NULL) {
+        (void)strncpy_s(deviceInfo.deviceId, sizeof(deviceInfo.deviceId), deviceId,
+            sizeof(deviceInfo.deviceId) - 1);
+    }
+    deviceInfo.deviceTypeId = (uint16_t)deviceTypeId;
+    LOGI("recv device_state state=%d isOnline=%d networkId=%s name=%s", state, isOnline, networkId, deviceName);
+    DeviceManagerNotify* notify = DmDeviceManagerNotifyGetInstance();
+    DmNotifyCallDevStateChange(notify, (DmDeviceState)state, &deviceInfo, isOnline ? true : false);
+    return DM_OK;
+}
+
+static int32_t DmHandleDeviceFoundCase(IpcIo* ipcData)
+{
+    int32_t subscribeId = 0;
+    ReadInt32(ipcData, &subscribeId);
+    size_t readLen = 0;
+    const char* deviceId = (const char*)ReadString(ipcData, &readLen);
+    const char* networkId = (const char*)ReadString(ipcData, &readLen);
+    const char* deviceName = (const char*)ReadString(ipcData, &readLen);
+    int32_t deviceTypeId = 0;
+    ReadInt32(ipcData, &deviceTypeId);
+    const char* extraData = (const char*)ReadString(ipcData, &readLen);
+    DmDeviceInfo deviceInfo;
+    (void)memset_s(&deviceInfo, sizeof(DmDeviceInfo), 0, sizeof(DmDeviceInfo));
+    if (deviceId != NULL) {
+        (void)strncpy_s(deviceInfo.deviceId, sizeof(deviceInfo.deviceId), deviceId,
+            sizeof(deviceInfo.deviceId) - 1);
+    }
+    if (networkId != NULL) {
+        (void)strncpy_s(deviceInfo.networkId, sizeof(deviceInfo.networkId), networkId,
+            sizeof(deviceInfo.networkId) - 1);
+    }
+    if (deviceName != NULL) {
+        (void)strncpy_s(deviceInfo.deviceName, sizeof(deviceInfo.deviceName), deviceName,
+            sizeof(deviceInfo.deviceName) - 1);
+    }
+    deviceInfo.deviceTypeId = (uint16_t)deviceTypeId;
+    if (extraData) {
+        DmStringAssignCstr(&deviceInfo.extraData, extraData);
+    }
+    LOGI("recv device_found subId=%d networkId=%s extraData=%s", subscribeId, networkId,
+        extraData ? extraData : "");
+    DeviceManagerNotify* notify = DmDeviceManagerNotifyGetInstance();
+    DmNotifyOnDeviceFound(notify, NULL, (uint16_t)subscribeId, &deviceInfo);
+    DmDeviceInfoDestroy(&deviceInfo);
+    return DM_OK;
+}
+
+static int32_t DmHandlePublishFinishCase(IpcIo* ipcData)
+{
+    int32_t publishId = 0;
+    int32_t result = 0;
+    ReadInt32(ipcData, &publishId);
+    ReadInt32(ipcData, &result);
+    LOGI("recv publish_finish publishId=%d result=%d", publishId, result);
+    DeviceManagerNotify* notify = DmDeviceManagerNotifyGetInstance();
+    DmNotifyOnPublishResult(notify, NULL, publishId, result);
+    return DM_OK;
+}
+
+DM_EXPORT int32_t DmIpcClientStubOnRemoteRequest(IpcClientStub* self, uint32_t code,
+    void* data, void* reply, void* option)
+{
+    (void)self;
+    (void)reply;
+    (void)option;
+    IpcIo* ipcData = (IpcIo*)data;
+    if (ipcData == NULL) {
+        return DM_OK;
+    }
+    LOGI("on_remote_request code=%u", code);
+    switch (code) {
+        case DM_SERVER_DEVICE_STATE_NOTIFY:
+            return DmIpcClientStubHandleDeviceState(ipcData);
+        case DM_SERVER_DEVICE_FOUND:
+            return DmHandleDeviceFoundCase(ipcData);
+        case DM_SERVER_PUBLISH_FINISH:
+            return DmHandlePublishFinishCase(ipcData);
+        case DM_SERVER_DISCOVER_FINISH: {
+            size_t readLen = 0;
+            const char* pkgName = (const char*)ReadString(ipcData, &readLen);
+            int32_t subscribeId = 0;
+            ReadInt32(ipcData, &subscribeId);
+            int32_t result = 0;
+            ReadInt32(ipcData, &result);
+            LOGI("recv discover_finish subscribeId=%d result=%d", subscribeId, result);
+            DeviceManagerNotify* notify = DmDeviceManagerNotifyGetInstance();
+            DmNotifyOnDiscoveryResult(notify, pkgName, (uint16_t)subscribeId, result);
+            break;
+        }
+        default:
+            LOGW("unknown code=%u", code);
+            break;
+    }
+    return DM_OK;
+}
+
+
+DM_EXPORT int32_t DmIpcClientStubSendCmd(IpcClientStub* self, int32_t cmdCode, void* req, void* rsp)
+{
+    (void)self;
+    (void)cmdCode;
+    (void)req;
+    (void)rsp;
+    return DM_OK;
+}
